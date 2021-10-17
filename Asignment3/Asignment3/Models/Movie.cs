329a3b87eabed1ed05db68fed1b187181dae416a
Febri Wijaya <tampuboloncarles21@gmail.com>
2021-10-18 06:04:47 +0700

    Adding Assignment 3

---
+++ Asignment3/Asignment3/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asignment3.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string genre { get; set; }
+        public string durations { get; set; }
+        public string releasedate { get; set; } 
+    }
+}
